993fce7d4f957b90a3559fed95552c97a8c9da73
Muhammad Rivaldi Fanani <muhammadrivaldifnni01@gmail.com>
2025-08-30 07:32:22 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\auth\LoginController;
 use App\Http\Controllers\auth\RegisterController;
-use App\Http\Controllers\Admin\ERaportController;
-use App\Http\Controllers\Admin\TugasController as AdminTugasController;
-use App\Http\Controllers\Guru\TugasController as GuruTugasController;
-use App\Http\Controllers\Siswa\TugasController as SiswaTugasController;
-use App\Http\Controllers\Guru\NilaiController as GuruNilaiController;
-use App\Http\Controllers\Siswa\NilaiController as SiswaNilaiController;
-use Telegram\Bot\Api;
 
 Route::middleware('guest')->group(function () {
     Route::controller(LoginController::class)->group(function () {
